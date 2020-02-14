8b8ca53f1247c723a25e73752f0411f658772c66
LeiKD <dkomen180@gmail.com>
2020-02-14 12:12:46 +0300

    Edit form

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 function day() {
+  var name= document.getElementById="name";
+  var day= document.getElementById="day";
+  var month= document.getElementById="month";
+  var year= document.getElementById="year";
+  var century=year.slice(0,2);
+  if (day===""|| day<1 || day>31 || month===""|| month<1 || month>12 || year===""|| ) {
+  alert("Please confirm that you have written the correct values")
+  } else {
+    ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
+  }   
+  }
+function gender() {
+  var male=document.getElementByName="male"
+  var male=document.getElementsByName="female"
   
 }
-function male() {
-
-}
-function female() {
-
+function getName() {
+  
 }
