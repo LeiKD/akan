55daadc35e1e0166472d4c97abb7d05283b65745
LeiKD <dkomen180@gmail.com>
2020-02-14 12:17:50 +0300

    Put limits on input text

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function getName() {
   var month= document.getElementById="month";
   var year= document.getElementById="year";
   var century=year.slice(0,2);
-  if (day===""|| day<1 || day>31 || month===""|| month<1 || month>12 || year===""|| ) {
+  if (day===0|| day<1 || day>31 || month===0| month<1 || month>12 || year===0| ) {
   alert("Please confirm that you have written the correct values")
   } else {
     ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
