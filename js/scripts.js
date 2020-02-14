3e4f904a083713fa10dbba8f5ef02d09fccaec6e
LeiKD <dkomen180@gmail.com>
2020-02-14 12:48:02 +0300

    Add formula

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ function getName() {
   var day= document.getElementById="day";
   var month= document.getElementById="month";
   var year= document.getElementById="year";
-  var century=year.slice(0,2);
+  var yr= year.slice(2,4);
+  var century= year.slice(0,2);
+  var result= ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
   if (day===0|| day<1 || day>31 || month===0| month<1 || month>12 || year===0| ) {
   alert("Please confirm that you have written the correct values")
   } else {
-    ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
+    alert(result)
   }   
   }
 function gender() {
