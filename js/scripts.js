01ab0d23636b4c19ae2851f006e34f2a645e8782
LeiKD <dkomen180@gmail.com>
2020-02-14 18:10:00 +0300

    Add initial alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-var akanFemale=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-var akanMale=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
-var daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 function getName() {
-  var name= document.getElementById="name";
-  var day= parseInt(document.getElementById="day").value;
-  var month= parseInt(document.getElementById="month").value;
-  var year= parseInt(document.getElementById="year").value;
-  var yr= year.slice(2,4);
-  var century= year.slice(0,2);
-  var result= ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7
-  if (day===0|| day<1 || day>31 || month===0| month<1 || month>12 || year===0| ) {
-  alert("Please confirm that you have written the correct values")
+  var dd= parseInt(document.getElementById("day").value);
+  var mm= parseInt(document.getElementById("month").value);
+  var yy= parseInt(document.getElementById("year").value);
+  var cc= (yy-1)/100 +1
+  var result= ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
+  var boy=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+  var girl=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  var siku=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  if (document.getElementsById("Gender").checked ) {
+    var gender=male;
   } else {
-    alert(result)
-  }   
+    var gender=female;
   }
-function gender() {
-  var male=document.getElementByName="male"
-  var male=document.getElementsByName="female"
+  if (dd<1 || dd>31) {
+    alert("Invalid Date!");
+  }
+  else if (mm<1 || mm>12 || mm==2 && dd>29) {
+    alert("Invalid Month!");
+  }
   
+ 
 }
